fa8c4d984b22da0508e9ec7f598fd8a8a5b33f7d
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-09-22 22:43:51 +0900

    block placement

---
+++ nextjs/app/components/Blocks.tsx
@@ -0,0 +1,17 @@
+import styles from "./Blocks.module.css";
+
+export function Blocks() {
+  return (
+    <div className={styles.component}>
+      <div className={styles.block}>1</div>
+      <div className={styles.block}>2</div>
+      <div className={styles.block}>3</div>
+      <div className={styles.block}>4</div>
+      <div className={styles.block}>5</div>
+      <div className={styles.block}>6</div>
+      <div className={styles.block}>7</div>
+      <div className={styles.block}>8</div>
+      <div className={styles.block}>9</div>
+    </div>
+  );
+}
--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,5 +1,10 @@
+import { Blocks } from "./components/Blocks";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>empty</div>;
+  return (
+    <div className={styles.component}>
+      <Blocks />
+    </div>
+  );
 }
